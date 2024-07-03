cfac849b660c804045c6fc03bb1de4b8b542c4b4
nahidul-fahim <nahidulfahim.official@gmail.com>
2024-07-03 22:13:31 +0600

    installed tailwind and set up project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      first: {
+        DEFAULT: "#4fff64",
+        dark: "#091e0c",
+        light: "#c1ffc8"
+      }
+      }
+    },
   },
   plugins: [],
 }
-
